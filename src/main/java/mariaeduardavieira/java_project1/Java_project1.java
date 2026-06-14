aedd01dc183cb23fbabedd8e2a702bef4f2403fa
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-06-14 16:51:14 -0300

    primeiro commit do meu projeto em java

---
+++ src/main/java/mariaeduardavieira/java_project1/Java_project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mariaeduardavieira.java_project1;
+
+/**
+ *
+ * @author Maria Eduarda
+ */
+public class Java_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Aqui quem fala é a Duda Hello World!!");
+    }
+}
