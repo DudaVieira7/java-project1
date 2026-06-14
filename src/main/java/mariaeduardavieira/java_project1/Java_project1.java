5ed38db6f833e209fbf70cfaecbb5129d8f3f815
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-06-14 18:24:57 -0300

    testes de uso de java

--- src/main/java/mariaeduardavieira/java_project1/Java_project1.java
+++ src/main/java/mariaeduardavieira/java_project1/Java_project1.java
@@ -4,13 +4,27 @@
 
 package mariaeduardavieira.java_project1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Maria Eduarda
  */
 public class Java_project1 {
+    
+    private final static String WELCOME_MESSAGE = "Inform your name please: ";
 
     public static void main(String[] args) {
-        System.out.println("Mudei o texto");
+        
+        // meu comentário de uma linha
+        /* comentário de multiplas linhas em java*/
+        
+        
+        var scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MESSAGE);
+        var name = scanner.next();
+        System.out.println("inform your age: ");
+        var age = scanner.nextInt();
+        System.out.printf("Hello %s your age is %s \n", name, age);
     }
 }
