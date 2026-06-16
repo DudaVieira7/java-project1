0b6a56b9677654bc5ba1cd25da795dc08a272e53
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-06-16 12:54:31 -0300

    estrutura de repetição for

---
+++ src/main/java/extruturas_de_controle/repeticao_for.java
@@ -0,0 +1,13 @@
+package extruturas_de_controle;
+
+
+public class repeticao_for{
+    public static void main(String[] args) {
+       
+        for(var i= 1; i <= 50 ; i ++){
+           System.out.println(i);
+           
+        } 
+        
+    }
+}
