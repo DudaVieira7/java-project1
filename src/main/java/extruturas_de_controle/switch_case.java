70da71551d52f8178489df73e58fb78db38fdac6
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-06-16 11:48:21 -0300

    estrutura condicional switch case

---
+++ src/main/java/extruturas_de_controle/switch_case.java
@@ -0,0 +1,22 @@
+package extruturas_de_controle;
+
+import java.util.Scanner;
+
+
+public class switch_case{
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("digite um numero de 1 a 7 para escolher uma mensagem: ");
+        var option = scanner.nextInt();
+        switch (option){
+            case 1 -> System.out.println("Domingo");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> System.out.println("Terça");
+            case 4 -> System.out.println("Quarta");
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            case 7 -> System.out.println("Sabado");
+            default -> System.out.println("Opcao invalida");
+        }
+    }
+}
