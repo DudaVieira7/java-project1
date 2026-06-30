3b38168b4d75751f3bd1eb0cbb85f641dee572d7
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-06-30 14:16:42 -0300

    herança e polimorfismo entre classes

--- src/main/java/herancaPolimorfismo/Main.java
+++ src/main/java/herancaPolimorfismo/Main.java
@@ -3,15 +3,47 @@ package herancaPolimorfismo;
 
 public class Main{
     public static void main(String[] args) {
-       
-        Manager manager = new Manager();
-        manager.setName("Maria");
-        manager.setLogin("maria123");
-        manager.setPassword("123456");
+      printEmployee(new Manager());
+      printEmployee(new SalesPerson());
+    }
+    public static void printEmployee(Employee employee){
         
-        System.out.println(manager.getName());
-        System.out.println(manager.getLogin());
-        System.out.println(manager.getPassword());
+        System.out.printf(("==============%s=================\n"),employee.getClass().getCanonicalName());
+        if (employee instanceof Manager){
+        
+      
+        }
+        switch (employee){                  // funciona com o switch case sem o default porque pegou todos os cenários possíveis.
+            case Manager manager ->{
+                employee.setCode("123");
+                employee.setName("Maria");
+                employee.setSalary(5500);
+                manager.setCommision(1200);
+                manager.setLogin("mariavieira");
+                manager.setPassword("123456");
+                
+                System.out.println(employee.getCode());
+                System.out.println(employee.getName());
+                 System.out.println(employee.getSalary());
+                 System.out.println(manager.getCommision());
+                System.out.println(manager.getLogin());
+                System.out.println(manager.getPassword());
+            }
+            case SalesPerson salesperson ->{
+                salesperson.setCode("123");
+                salesperson.setName("João");
+                salesperson.setSalary(2800);
+                salesperson.setPercentPerSold(25);
+             
+                
+                System.out.println(employee.getCode());
+                System.out.println(employee.getName());
+                System.out.println(employee.getSalary());
+                
+            
+            }
+        }
+         System.out.println("==============================================");
     }
 
 }
