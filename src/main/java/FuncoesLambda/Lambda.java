4cd7279f31c4c31ce003e07ac919f17f27eae195
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-07-08 22:22:23 -0300

    filtrando palavras com função lambda e o filter

--- src/main/java/FuncoesLambda/Lambda.java
+++ src/main/java/FuncoesLambda/Lambda.java
@@ -4,6 +4,10 @@
  */
 package FuncoesLambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Maria Eduarda
@@ -13,6 +17,10 @@ interface OperacaoMatematica{
     int calcular(int a , int b);
 }
 
+
+interface ManipuladorString{
+    String manipular(String str);
+}
 
 public class Lambda {
     public static void main(String[] args) {
@@ -44,6 +52,53 @@ public class Lambda {
         
          System.out.println(multiplicacao.calcular(5, 7));
           System.out.println("Multiplicação com função lambda: " + multiplicacao.calcular(5, 7));
+          
+          
+          
+          
+          
+          
+           // manipulando Strings com funções lambda
+           
+            ManipuladorString maiuscula = new  ManipuladorString(){
+            @Override
+            public String manipular(String str) {                       // formato sem lambda 
+
+                return str.toUpperCase();
+            }
+            };
+            
+            System.out.println("Manipulação de String:  (sem lambda )" + maiuscula.manipular("Fada da computacao"));
+            
+            
+          
+            ManipuladorString maiusculaFL = str -> str.toUpperCase();       // formado com lambda
+            System.out.println("Manipulação de String:  (com lambda )" + maiusculaFL.manipular("Fada da computacao"));
+            
+            
+            
+            
+            
+            // usando  o filter
+            
+            List<String> palavras = Arrays.asList("java","python","typeScript","javaScript",
+                    "Julia","Php","SQL","C++","Go","Rust","Swift","Ruby","Dart","C#");
+            
+            List<String> palavrasFiltradas = new ArrayList<>();
+            
+            for(String palavra : palavras){
+                if(palavra.length() > 5){                                   // método sem função lambda
+                    palavrasFiltradas.add(palavra);
+                }
+            }
+            
+          System.out.println("Palavras com mais de cinco letras filtrada sem função lambda" + palavrasFiltradas);
+          
+          
+           List<String> palavrasFiltradasFL = palavras.stream().filter(s -> s.length() > 5).toList();    // ultilizando a função lambda.
+           
+           System.out.println("Palavras com mais de cinco letras filtrada com função lambda: " + palavrasFiltradasFL);
+          
     }
     
 }
